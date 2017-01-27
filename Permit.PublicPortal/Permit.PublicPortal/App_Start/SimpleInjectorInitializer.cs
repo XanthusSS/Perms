72670a39d3e4a0dd8acfc0b839e172c6fbd98235
ravi <ravi@RAVANYOGAPRO3>
2017-01-27 16:48:46 +0530

    Add project files.

---
+++ Permit.PublicPortal/Permit.PublicPortal/App_Start/SimpleInjectorInitializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Permit.PublicPortal.App_Start
+{
+    public class SimpleInjectorInitializer
+    {
+    }
+}
---
+++ Permit.PublicPortal/Permit.PublicPortal/Models/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Permits.DataAccess.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Permit.PublicPortal
+{
+    public class RegisterViewModel
+    {
+        public RegisterViewModel()
+        {
+           User = new UserSignUpView();
+           Car = new List<CarViewModel>();
+        }
+        public UserSignUpView User { get; set; }
+
+        public List<CarViewModel> Car { get; set; }
+    }
+}
